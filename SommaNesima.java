81afc06a1015f082eb1fcb23398aa418f8d64421
Lorenzo <35295037+LorenzoElSalserito@users.noreply.github.com>
2018-03-23 17:39:44 +0100

    Create SommaNesima.java

---
+++ SommaNesima.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Esempio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int s=0,a,b,n,i;
+Scanner inputNumScanner = new Scanner(System.in);
+
+System.out.println("Quanti numeri sommare? ");
+n = inputNumScanner.nextInt();
+
+for(i=0, i<n, i++){
+System.out.println("Inserisci n-esimo numero: ");
+b = inputNumScanner.nextInt();
+
+  }
+ }
+}
